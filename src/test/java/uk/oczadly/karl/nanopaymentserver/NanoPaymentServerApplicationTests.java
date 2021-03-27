770070cccf91092fea46567798b199fcca2f224b
Karl Oczadly <karl@oczadly.uk>
2021-03-27 14:28:20 +0000

    Initial commit.

---
+++ src/test/java/uk/oczadly/karl/nanopaymentserver/NanoPaymentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.oczadly.karl.nanopaymentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanoPaymentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
